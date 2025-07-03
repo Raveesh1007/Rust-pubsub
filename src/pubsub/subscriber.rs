e56579e08719fb45a04f7ac60c66626a422a8ca3
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2025-07-03 15:06:43 +0530

    mod subscriber.rs

--- src/pubsub/subscriber.rs
+++ src/pubsub/subscriber.rs
@@ -1,8 +1,8 @@
-use tokio::sync::{broadcast::Reciever, mpsc::Sender};
+use tokio::sync::{broadcast::Receiver, mpsc::Sender};
 
-pub async fn subscribe(mut receiver: Receiver<String>, sender: Sender<String>){
-    while let Ok(message) = receiver.recv().await{
-        if sender.send(message).await.is_err(){
+pub async fn subscribe(mut receiver: Receiver<String>, sender: Sender<String>) {
+    while let Ok(msg) = receiver.recv().await {
+        if sender.send(msg).await.is_err() {
             break;
         }
     }
