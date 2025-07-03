66c962db5e163c8992d017f149f9b0f3afae02da
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2025-07-03 15:06:10 +0530

    updated message.rs

--- src/pubsub/message.rs
+++ src/pubsub/message.rs
@@ -1,53 +1,55 @@
-use serde:: {Deserialize, Serialize};
+use serde::{Deserialize, Serialize};
 use tokio::sync::{broadcast, mpsc};
 
 #[derive(PartialEq, Eq, Hash, Debug, Deserialize)]
 #[serde(rename_all = "lowercase")]
-
-pub enum Message{
-    Subscribe{
-        topic: Vec<String>,
+pub enum PubSubRequest {
+    Subscribe {
+        topics: Vec<String>,
     },
-    Unsubscribe{
-        topic: Vec<String>,        
+    Unsubscribe {
+        topics: Vec<String>,
     },
-    Publish{
-        topic: Vec<String>,
-        data: String,
+    Publish {
+        topics: Vec<String>,
+        message: String,
         key: Option<String>,
     },
 }
 
-
 #[derive(PartialEq, Eq, Hash, Debug, Serialize)]
 #[serde(rename_all = "lowercase")]
-pub enum PubSubResponse{
-    Message{topic: String, message: String},
-    Error{ error: String},
+pub enum PubSubResponse {
+    Message { topic: String, message: String },
+    Error { message: String },
 }
 
-pub fn process_subscription_message(message: impl ToString) -> serde_json::Result<PubSubResponse>{
+pub fn process_subscription_message(message: impl ToString) -> serde_json::Result<PubSubRequest> {
     serde_json::from_str(&message.to_string())
 }
 
-pub fn send_message(topic: &impl ToString, message: impl ToString, sender: &broadcast::Sender<String>) 
--> Result<(), Box<dyn std:: error:: Error>>{
-    let message = PubSubResponse::Message{
+pub fn send_message(
+    topic: &impl ToString,
+    message: &impl ToString,
+    sender: &broadcast::Sender<String>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let message = PubSubResponse::Message {
         topic: topic.to_string(),
         message: message.to_string(),
     };
-
     let message = serde_json::to_string(&message)?;
     sender.send(message)?;
     Ok(())
 }
 
-pub async fn send_error(message: impl ToString, sender: mpsc::Sender<String>,
-) -> Result<(), Box<dyn std::error::Error>>{
-    let message = PubSubResponse::Error{
+pub async fn send_error(
+    message: impl ToString,
+    sender: mpsc::Sender<String>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let message = PubSubResponse::Error {
         message: message.to_string(),
     };
     let message = serde_json::to_string(&message)?;
-    sender.send(message)?;
+    sender.send(message).await?;
     Ok(())
 }
